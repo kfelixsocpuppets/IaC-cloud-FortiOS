7c961ca57f3fcf7ec25220846795c72f229a78c6
kfelixsocpuppets <59031696+kfelixsocpuppets@users.noreply.github.com>
2025-11-12 12:37:05 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,13 +1,15 @@
 # variables.tf
 
+# all variables should be defined in a variable.tf
+
 variable "hostname_value" {
-  description = "The desired hostname for the resource"
+  description = "The desired hostname for the specifc resource"
   type        = string
 }
 
 
 variable "token" {
-  description = "The desired API-token  for the resource"
+  description = "The desired API-token  for the specifc resource"
   type        = string
 }
 
