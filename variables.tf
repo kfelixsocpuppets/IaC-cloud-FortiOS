27f398a674dee7774490c8fc9207124208f1daf3
kfelixsocpuppets <59031696+kfelixsocpuppets@users.noreply.github.com>
2025-11-12 12:25:25 -0600

    Create variables.tf
    Sample Variables file

---
+++ variables.tf
@@ -0,0 +1,13 @@
+# variables.tf
+
+variable "hostname_value" {
+  description = "The desired hostname for the resource"
+  type        = string
+}
+
+
+variable "token" {
+  description = "The desired API-token  for the resource"
+  type        = string
+}
+
