58581b25ae868a110d02e0a107c3346b5607b9b1
kfelixsocpuppets <59031696+kfelixsocpuppets@users.noreply.github.com>
2025-11-12 12:21:10 -0600

    Create main.tf
    for demo purpose

---
+++ main.tf
@@ -0,0 +1,12 @@
+# dummy address for holding DO NOT REMOVE 
+
+resource "fortios_firewall_address" "dumbass" {
+
+  name   = "dumbass"
+
+  subnet = "169.254.66.66/32"
+  
+  comment = " Managed by TERRAFORMS BLOGGER DEMO "
+
+}
+
